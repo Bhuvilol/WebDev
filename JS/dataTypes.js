1af44b92888771ba033618db5cd0f784a4d79030
Bhabesh <bhabeshcse@gmail.com>
2024-08-05 17:52:01 +0530

    5 aug

---
+++ JS/dataTypes.js
@@ -0,0 +1,17 @@
+"use strict";     // treat all JS code as newer version
+
+// alert(3+3)         deprecated(we are using nodejs, not browser)
+let name = "Bhabesh"
+let age = 18
+let isLoggedIn = false
+
+//number(int)
+//bigint(long)
+//string =>""
+//boolean => true/false
+//null => standalone value  (null is a type of object)
+//undefined =>
+//symbol => unique
+//object
+
+console.log(typeof null);
---
+++ JS/variables.js
@@ -0,0 +1,16 @@
+const accountID = 144553;
+let accountEmail = "bhabeshcse@gmail.com";
+var accountPass = "12345";
+accountCity = "Jaipur";
+let accountState ;
+// accountID = 2 \                - const variable can not be changed
+accountEmail = "likugpr1999@gmail.com"
+accountPass = "98765"
+accountCity = "Bhonsar"
+
+console.log(accountID);
+console.table([accountID,accountEmail,accountPass,accountCity,accountState])
+
+/*
+Prefer not to use var because of issue in block acope and functional scope
+*/
