16e6c1667b4d74313ce57fa7960c8adec462807c
Bhabesh <bhabeshcse@gmail.com>
2024-08-17 15:06:36 +0530

    script files

---
+++ JS/basics3/function.js
@@ -0,0 +1,13 @@
+function sayMyName(){
+    console.log("My name is Bhabesh");
+}
+sayMyName();
+
+function addTwoNumber(number1,number2) {
+    return number1 + number2;
+}
+console.log(addTwoNumber(10,20));
+
+
+
+
