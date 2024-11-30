bd2c1e7a6d9c88e98682241ce9e26d0325fae9f3
Bhabesh <bhabeshcse@gmail.com>
2024-12-01 02:05:01 +0530

    logic in js

---
+++ JS/ControlFlow/switch.js
@@ -0,0 +1,27 @@
+switch (month) {
+    case 1:
+        console.log("January");
+        
+        break;
+    case 2:
+        console.log("February");
+        
+        break;
+    case 3:
+        console.log("March");
+        
+        break;
+    case 4:
+        console.log("April");
+        
+        break;
+    default:
+        break;
+}
+
+//falsy values : 
+// false, 0, -0, BigInt On, NaN, null, undefined, "", ''
+
+//truthy values : 
+// true, 1, 1n, BigInt Off, NaN, null, undefined, "", ''\
+//"0", 'false'," ". ' '
