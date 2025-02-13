8f735c29d418485c0bbdbad7d599d46c79e9dea3
Bhabesh <bhabeshcse@gmail.com>
2025-02-13 11:20:35 +0530

    oops in js

--- JS/classes_and_oops/oop.js
+++ JS/classes_and_oops/oop.js
@@ -17,3 +17,15 @@ console.log(user.getuserdetails());
 
 const promiseOne = new Promise()
 const date = new Date()
+
+function User(username,loginCount,isLoggedIn){
+    this.username = username
+    this.loginCount = loginCount
+    this.isLoggedIn = isLoggedIn
+    this.getuserdetails = function(){
+        console.log(`Username is ${this.username}`);
+        console.log(`Login count is ${this.loginCount}`);
+        console.log(`User is logged in ${this.isLoggedIn}`);
+    return this;
+    }
+}
