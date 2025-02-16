3ac30d2c1c3e1c15004ad1338f900fa5279bfbfc
Bhabesh <bhabeshcse@gmail.com>
2025-02-16 12:18:11 +0530

    usage of usesstate hook in react

--- REACT/01/src/Content.js
+++ REACT/01/src/Content.js
@@ -1,35 +1,41 @@
+import { useState } from 'react';
 
 const Content = () => {
-    const handleNameChange = () => {
-        const names = ['Bob','Kevin','Dave']
-        const int = Math.floor(Math.random() * 3)
-        return names[int];
-    }
+  const [name, setName] = useState('Dave') 
+  // const [count, setCount] = useState(0)
 
-    const handleClick = () => {
-        console.log('You clicked it!')
-    }
-    const handleClick2 = (name) => {
-        console.log(`${name} clicked it!`)
-    }
-    const handleClick3 = (e) => {
-        console.log(e.target.innerText)
-    }
+  // const handleNameChange = () => {
+  //       const names = ['Bob','Kevin','Dave']
+  //       const int = Math.floor(Math.random() * 3)
+  //       setName(names[int]);
+  //   }
+
+    // const handleClick = () => {
+    //     setCount(count+1)
+    //     console.log(count);
+        
+    // }
+    // const handleClick2 = () => {
+    //     console.log(count)
+    // }
+    // const handleClick3 = (e) => {
+    //     console.log(e.target.innerText)
+    // }
   return (
     
     <main>
-        <p onDoubleClick={handleClick}>
-            Hello {handleNameChange()}!
+        {/* <p onDoubleClick={handleClick}>
+            Hello {name}!
         </p>
+        <button onClick={handleNameChange}>
+          Change Name
+        </button>
         <button onClick={handleClick}>
           Click it
         </button>
-        <button onClick={()=>handleClick2('Dave')}>
+        <button onClick={handleClick2}>
           Click it
-        </button>
-        <button onClick={(e)=>handleClick3(e)}>
-          Click it
-        </button>
+        </button> */}
     </main>
   )
 }
